d0ada311316314d421c211234dedf4edf7e3746e
davidstrasak <davidstrasak@seznam.cz>
2025-02-08 13:38:57 +0100

    paste the code that has both relay control and LCD screen that I made before

---
+++ include/serverRouter.h
@@ -0,0 +1,7 @@
+// webRoutes.h
+#ifndef WEB_ROUTES_H
+#define WEB_ROUTES_H
+
+void handleWebRequests();  // Example function
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,129 @@
 #include <Arduino.h>
-/*
-* Using LCD screen with wemos D1 Mini and Arduino
-* Written by Ahmad Shamshiri on Aug 01, 2019
-* at 17:42 in Ajax, Ontario, Canada
-* for Robojax.com
-* Watch video instruction for this code: https://youtu.be/mRAgS-CdURU
-*
-Get this code and other Arduino codes from Robojax.com
-If you found this tutorial helpful, please support me so I can continue creating
-content like this. You can support me on Patreon http://robojax.com/L/?id=63
-or make donation using PayPal http://robojax.com/L/?id=64
-
-*/
-
-// start of settings for LCD1602 with I2C
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFiClient.h>
 #include <Wire.h>
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
+
+// Global variables for WiFi credentials
+const char* wifiNetworkName = "TP-Link_83CA";
+const char* wifiNetworkPassword = "65362280";
+
+// Initialize the web server at port 80
+ESP8266WebServer webServer(80);
+
+// Relay control pin
+#define relayControlPin D6
+
+// Create an LCD instance with address 0x27 for a 16x2 display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-// end of settings for LCD1602 with I2C
 
-int count = 0;
+int counter = 0;
+
+// Route handler for the main page
+void mainRoute() {
+   String secondsSinceStart = String(millis() / 1000);
+   String response = "Hello Arduino World!<br>";
+   response += "<br>Time since Arduino started: ";
+   response += secondsSinceStart;
+   response += " seconds.<br><br>";
+   response += "LED status: ";
+   response += digitalRead(relayControlPin);
+   response += "<br><br>";
+   response += "<a href=\"/ledON\">Turn LED ON</a><br><br>";
+   response += "<a href=\"/ledOFF\">Turn LED OFF</a>";
+
+   webServer.send(200, "text/html", response);
+}
+
+// Route handler for unknown pages
+void unknownRouteResponse() {
+   String response = "Non-existing route\n\n";
+   response += "URI: ";
+   response += webServer.uri();
+   response += "\nMethod: ";
+   response += (webServer.method() == HTTP_GET) ? "GET" : "POST";
+   response += "\nArguments: ";
+   response += webServer.args();
+   response += "\n";
+   for (uint8_t i = 0; i < webServer.args(); i++) {
+      response += " " + webServer.argName(i) + ": " + webServer.arg(i) + "\n";
+   }
+   webServer.send(404, "text/plain", response);
+}
+
 void setup() {
+   // Configure relay pin as output and set it LOW
+   pinMode(relayControlPin, OUTPUT);
+   digitalWrite(relayControlPin, LOW);
 
-   pinMode(A0, INPUT);
-   pinMode(D0, INPUT);
-   // pinMode(D1, OUTPUT); // D1 is used for I2C
-   // pinMode(D2, OUTPUT); // D2 is used for I2C
-   pinMode(D3, INPUT);
-   pinMode(D4, INPUT);
-   pinMode(D5, OUTPUT);
-   pinMode(D6, OUTPUT);
-   pinMode(D7, OUTPUT);
-   pinMode(D8, OUTPUT);
-   pinMode(LED_BUILTIN, OUTPUT);
+   // Start serial communication
+   Serial.begin(115200);
 
-   digitalWrite(D5, LOW);
-   digitalWrite(D6, LOW);
-   digitalWrite(D7, LOW);
-   digitalWrite(D8, LOW);
+   // Start WiFi communication with credentials
+   WiFi.begin(wifiNetworkName, wifiNetworkPassword);
 
+   // Wait until connected to the network, printing dots
+   while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+   }
+   Serial.println("");
+   Serial.print("Connected to WiFi network ");
+   Serial.println(wifiNetworkName);
+   Serial.print("IP address: ");
+   Serial.println(WiFi.localIP());
+
+   // Check MDNS functionality
+   if (MDNS.begin("espwebserver")) {
+      Serial.println("MDNS responder started.");
+   }
+
+   // Assign routes
+   webServer.on("/", mainRoute);
+
+   webServer.on("/example", []() {
+      String response = "Sample route for multiple pages.";
+      webServer.send(200, "text/plain", response);
+   });
+
+   webServer.on("/ledON", []() {
+      digitalWrite(relayControlPin, HIGH);
+      Serial.println("Turning LED ON.");
+      mainRoute();
+   });
+
+   webServer.on("/ledOFF", []() {
+      digitalWrite(relayControlPin, LOW);
+      Serial.println("Turning LED OFF.");
+      mainRoute();
+   });
+
+   webServer.onNotFound(unknownRouteResponse);
+
+   // Start the HTTP server
+   webServer.begin();
+   Serial.println("HTTP server started.");
+
+   // Initialize and set up the LCD
    lcd.init();
    lcd.backlight();
-   lcd.print("Uz to jede");
-   lcd.setCursor(0, 1);
-   lcd.print("Demo");
-   delay(2000);
+   lcd.print("Robojax D1 Mini");
 }
 
 void loop() {
-   // Robojax D1 Mini LCD-1602-I2C
-   lcd.clear();  // clear previous values from screen
-   lcd.print("Funguje to.");
+   // Handle incoming web requests
+   webServer.handleClient();
+   delay(10);
+
+   // Update the LCD display
+   lcd.clear();
+   lcd.print(WiFi.localIP());
    lcd.setCursor(0, 1);
    lcd.print("Counting:");
-   lcd.setCursor(11, 1);
-   lcd.print(count);
-   count++;
+   lcd.setCursor(9, 1);
+   lcd.print(counter);
+   counter++;
    delay(200);
-
-   // Basic LED blink on D5 pin
-   digitalWrite(LED_BUILTIN, HIGH);
-   delay(500);
-   digitalWrite(LED_BUILTIN, LOW);
-   delay(500);
 }
